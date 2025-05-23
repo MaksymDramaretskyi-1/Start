ab39fd6a329cd60b9542cc0a25556c9709a00f71
MaksymDramaretskyi-1 <maksym.glodedata@gmail.com>
2025-05-23 14:50:48 +0200

    Develompent website "Start"-sample

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav");
+  
+    burger.addEventListener("click", () => {
+      burger.classList.toggle("open");
+      nav.classList.toggle("active");
+    });
+  });  
